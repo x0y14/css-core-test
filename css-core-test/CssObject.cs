ef5fae86ff52ff631f8ac8d82ee00074d12dcb58
Yuhei Yamauchi <x0y14@macbook.local>
2020-12-09 00:54:47 +0900

    ?

--- css-core-test/CssObject.cs
+++ css-core-test/CssObject.cs
@@ -1,10 +1,34 @@
 ﻿using System;
-namespace csscoretest
+using System.Collections;
+
+public class CssObject
 {
-    public class CssObject
+    public string ObjectId { get; set; }
+    public Hashtable Style { get; set; }
+    public string StyleHtml { get; set; }
+
+    public CssObject(string oid)
     {
-        public CssObject()
-        {
-        }
+        ObjectId = oid;
+        Style = new Hashtable();
     }
+
+    public void UpdateStyle(string key, string value)
+    {
+        Style.Add(key, value);
+    }
+
+    public string OutPutStyle()
+    {
+        var html = $@".{ObjectId}{{ ";
+        foreach (DictionaryEntry st in Style)
+        {
+            //Console.WriteLine("Key = {0}, Value = {1}", st.Key, st.Value);
+            html += $@"{st.Key}: {st.Value}; ";
+        }
+
+        html += $@"}}";
+        return html;
+    }
+
 }
